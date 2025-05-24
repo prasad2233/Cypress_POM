9c99aa85869216d233105311551dce60979d8578
prasad2233 <prasaddurga3042@gmail.com>
2025-05-24 11:25:15 +0530

    assertions

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,7 +5,7 @@ describe("Login test cases", () => {
         cy.visit('https://www.saucedemo.com/')
     })
     it("Login with valid creds", () => {
-
+        cy.document().its('contentType').should('eq', 'text/html')
         cy.get('#user-name').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
         cy.get('#login-button').click()
@@ -17,5 +17,23 @@ describe("Login test cases", () => {
         cy.get('#login-button').click()
         cy.get('#react-burger-menu-btn').click()
         cy.get('[data-test="logout-sidebar-link"]').click()
+    })
+
+    it("Assertions in cypress", () => {
+        cy.get('div#login_credentials h4').then((element) => {
+            expect(element.text()).to.equal('Accepted usernames are:')
+        })
+        cy.get('div#login_credentials h4').contains('Accepted usernames are:').then((element) => {
+            expect(element.text()).to.equal('Accepted usernames are:')
+        })
+        cy.get('div#login_credentials h4').should('have.text', "Accepted usernames are:")
+        cy.get('div#login_credentials h4').should('contain', "Accepted usernames are:")
+        cy.get('#user-name').should('be.visible').type('standard_user')
+        cy.get('#user-name').should('have.attr', 'value', 'standard_user')
+        cy.get('div#login_credentials h4').should('have.html', "Accepted usernames are:")
+        cy.get('#user-name').should('have.attr', 'value').and("include", "standard_user")
+        cy.get('div#login_credentials h4').contains('Accepted usernames are:').then((element) => {
+            expect(element.text()).to.have.length(23);
+        })
     })
 })
